d23e233753a7e2affeed22c2f5804f9f8fb1e36b
kajih <kajih@wsl>
2021-11-25 20:49:55 +0100

    treesitter additional

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,9 @@ require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true
   },
+  indent = {
+    enable = true
+  },
   rainbow = {
     enable = true,
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
