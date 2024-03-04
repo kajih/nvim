b60d9ddf08d4638a7c421b7d18a0b0b1a8645315
Johan Häggqvist <kajih@GUNGNIR.lan>
2024-03-04 20:38:26 +0100

    restored lir and removed netrw

--- init.lua
+++ init.lua
@@ -175,8 +175,6 @@ require('lazy').setup {
   { import = 'custom.plugins' },
 }
 
-require 'netrw'
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 --
